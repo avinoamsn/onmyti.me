164ab2405398ebc423509de6fa6890e878732dfb
Avi Nerenberg <devavmeister@gmail.com>
2020-12-23 23:00:50 -0600

    fix(typescript): adds fix for \'types\' defs issue

--- .redwood/index.d.ts
+++ .redwood/index.d.ts
@@ -1,4 +1,4 @@
-// 2020-12-24T04:56:23.982Z
+// 2020-12-24T05:00:18.826Z
 /// <reference path="./types/import-dir-schemas.d.ts" />
 /// <reference path="./types/import-dir-services.d.ts" />
 /// <reference path="./types/routes.d.ts" />
