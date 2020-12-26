7b1f2beb8b1b94000011e07dae0b21485de03bed
Avi Nerenberg <devavmeister@gmail.com>
2020-12-25 20:30:12 -0600

    feat(entry): auto height resize on text-wrap

--- web/src/components/Entry/Entry.tsx
+++ web/src/components/Entry/Entry.tsx
@@ -1,10 +1,40 @@
-import { useEffect } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
-import { returnSVG } from 'src/assets'
+// import { returnSVG } from 'src/assets'
 import { usePersistentState } from 'src/hooks'
+
+/**
+ * TODO improve this logic to be more declarative
+ *
+ * Uses canvas.measureText to compute and return the width of the given text of given font in pixels.
+ *
+ * @param {String} text The text to be rendered.
+ * @param {String} font The css font descriptor that text is to be rendered with (e.g. "bold 14px verdana").
+ *
+ * @see https://stackoverflow.com/a/21015393/5425359
+ */
+function getTextWidth(text) {
+  // re-use canvas object for better performance
+  const canvas =
+    (getTextWidth as any).canvas ||
+    ((getTextWidth as any).canvas = document.createElement('canvas'))
+  const context = canvas.getContext('2d')
+  // context.font = font
+  const metrics = context.measureText(text)
+  console.log(metrics)
+  return metrics.width
+}
 
 export const Entry: React.FC<{ isFocused: boolean }> = ({ isFocused }) => {
   const [entryText, setEntryText] = usePersistentState(`entryText`, ``)
+
+  // numLines – re-calculated every time the input value changes
+  const [numLines, setNumLines] = useState(1)
+  useEffect(() => setNumLines(Math.ceil(getTextWidth(entryText) / 239) || 1), [
+    entryText,
+  ])
+
+  console.log(numLines)
 
   // re-focus input when `Today` is focused (autoFocus only covers page load)
   useEffect(() => {
@@ -21,7 +51,7 @@ export const Entry: React.FC<{ isFocused: boolean }> = ({ isFocused }) => {
         }
         value={entryText}
         onChange={(e) => setEntryText(e.target.value)}
-        rows={1}
+        rows={numLines}
         cols={50}
         wrap="hard"
         autoFocus
@@ -35,6 +65,14 @@ export const Entry: React.FC<{ isFocused: boolean }> = ({ isFocused }) => {
       <label htmlFor="current-entry-input">
         return &crarr; to save your note
       </label>
+
+      {/* used to calculate the width (and textwrap) for the textarea above */}
+      <div
+        id="current-entry-input-width"
+        className="absolute hidden whitespace-nowrap"
+      >
+        {entryText}
+      </div>
     </form>
   )
 }
